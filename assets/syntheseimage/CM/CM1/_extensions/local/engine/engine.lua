774cc05102c6b94c0aff93a0144987467253cee7
nsv <nathan.sanchizviel@pm.me>
2026-02-23 14:23:09 +0100

    test

---
+++ assets/syntheseimage/CM/CM1/_extensions/local/engine/engine.lua
@@ -0,0 +1,83 @@
+-- Filter called for each header
+function Header(element)
+    -- ------------------
+   
+    -- Check class
+    if not header_class(element) then return nil; end
+
+    -- Parameter
+    add_parameter(element)
+
+    -- argument
+    add_argument(element)
+
+    -- ------------------
+    return element
+end
+
+function header_class(element)
+    -- ------------------
+
+    -- lvl2 uniquement
+    if element.level ~= 2 then return false end
+
+    -- doit contenir la classe "engine"
+    for _, c in ipairs(element.attr.classes) do
+        if c == "engine" then
+            return true
+        end
+    end
+
+    return false
+
+    -- ------------------
+end
+
+function add_parameter(element)
+    -- ------------------
+
+    local attribut = element.attr.attributes
+    attribut["data-background-interactive"] = "true"
+    attribut["data-state"] = "hide-menubar"
+
+    -- ------------------
+end
+
+function add_argument(element)
+    -- ------------------
+
+    local attrs = element.attr.attributes
+    local base = "_extensions/local/engine/engine.html"
+    local params = {}
+
+    -- ---- OBJ ----
+    local obj = attrs["obj"]
+    if obj ~= nil then
+        table.insert(params, "obj=" .. obj)
+        attrs["obj"] = nil
+    end
+
+    -- ---- EDL ----
+    local edl = attrs["edl"]
+    if edl ~= nil then
+        table.insert(params, "edl=" .. edl)
+        attrs["edl"] = nil
+    end
+
+    -- ---- Autres futurs paramètres ici ----
+    -- local grid = attrs["grid"]
+    -- if grid ~= nil then
+    --   table.insert(params, "grid=" .. grid)
+    --   attrs["grid"] = nil
+    -- end
+
+    -- Construction finale de l'URL
+    local iframe = base
+        if #params > 0 then
+        iframe = iframe .. "?" .. table.concat(params, "&")
+    end
+
+    attrs["data-background-iframe"] = iframe
+
+    -- ------------------
+end
