cccb54b3ceb026d3227a332552a45c836bd4a337
nsv <nasavi@pm.me>
2026-08-01 12:41:13 +0200

    add contact

--- content/site/language.js
+++ content/site/language.js
@@ -3,22 +3,7 @@
     const path = window.location.pathname;
 
 
-    const localizedPaths = {
-      fr: {
-        "/fr/a-propos/": "/fr/a-propos/",
-        "/en/about/": "/fr/a-propos/",
-      },
-      en: {
-        "/fr/a-propos/": "/en/about/",
-        "/en/about/": "/en/about/",
-      },
-    };
-
     const translatedPath = (language) => {
-      const localizedPath = localizedPaths[language][path];
-      if (localizedPath) {
-        return localizedPath;
-      }
 
       if (path === `/${language}` || path.startsWith(`/${language}/`)) {
         return path;
